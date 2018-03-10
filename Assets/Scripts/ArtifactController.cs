73c31428b90c12b5f72b97ac4c542dd36584e961
Nicole <nlin.nicole@gmmail.com>
2018-03-09 22:06:38 -0500

    started artifact controller

---
+++ Assets/Scripts/ArtifactController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactController : MonoBehaviour {
+
+    public GameObject cam;
+    public GameObject vase;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Found artifact");
+            vase.transform.position = cam.transform.position;
+        }
+    }
+}
--- Assets/Scripts/SummonController.cs
+++ Assets/Scripts/SummonController.cs
@@ -31,11 +31,12 @@ public class SummonController : MonoBehaviour {
         {
             if(spawn[index].tag == "BridgeOnly")
             {
+                GameObject clone;
                 Debug.Log("bridge only");
                 Vector3 bridgeLoc = spawn[index].transform.position;
                 bridgeLoc.y += 6.6f; //add position offset
-                bridge.transform.position = bridgeLoc;
-                bridge.SetActive(true);
+                clone = Instantiate(bridge, bridgeLoc, Quaternion.Euler(-90, 0, 0));
+                clone.SetActive(true);
                 ++index;
             }
             
@@ -44,10 +45,12 @@ public class SummonController : MonoBehaviour {
         {
             if (spawn[index].tag == "BigB_Portal")
             {
+                GameObject clone;
                 Debug.Log("big bridge");
                 Vector3 bigBridgeLoc = spawn[index].transform.position;
-                bigBridgeLoc.y += 5;
-                bigBridge.SetActive(true);
+                bigBridgeLoc.y += 4;
+                clone = Instantiate(bigBridge, bigBridgeLoc, Quaternion.Euler(-90, 90, 0));
+                clone.SetActive(true);
                 ++index;
             }
         }
@@ -55,9 +58,11 @@ public class SummonController : MonoBehaviour {
         {
             if (spawn[index].tag == "Stairs_Portal")
             {
+                GameObject clone;
                 Debug.Log("stairs");
-                stairs.transform.position = spawn[index].transform.position;
-                stairs.SetActive(true);
+                Vector3 stairsLoc = spawn[index].transform.position;
+                clone = Instantiate(stairs, stairsLoc, Quaternion.Euler(-90, 180, 0));
+                clone.SetActive(true);
                 ++index;
             }
         }
@@ -66,21 +71,22 @@ public class SummonController : MonoBehaviour {
             // Stairs and Raised Portal
             if (spawn[index].tag == "Stairs_Portal") 
             {
+                GameObject clone1, clone2;
                 Debug.Log("raised portal");
 
                 //portal enter
                 Vector3 portalEnterLoc = spawn[index].transform.position;
                 portalEnterLoc.x -= 8f;
                 portalEnterLoc.y += 1.0f;
-                portalEnter.transform.position = portalEnterLoc;
-                portalEnter.SetActive(true);
+                clone1 = Instantiate(portalEnter, portalEnterLoc, Quaternion.Euler(-90, 90, 0));
+                clone1.SetActive(true);
 
                 //portal exit
                 Vector3 portalExitLoc = spawn[index].transform.position;
                 portalExitLoc.x += 8f;
                 portalExitLoc.y += 7f;
-                portalExit.transform.position = portalExitLoc;                
-                portalExit.SetActive(true);
+                clone2 = Instantiate(portalExit, portalExitLoc, Quaternion.Euler(-90, 90, 0));
+                clone2.SetActive(true);
 
                 GameObject player = GameObject.Find("Player");
                 CharController charControl = player.GetComponent<CharController>();
